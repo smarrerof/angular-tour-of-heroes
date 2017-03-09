d2efb398751948517f0c2f7a340434729c3407bb
Sergio <smarrerof@gmail.com>
2017-03-09 20:37:00 +0000

    Finished 'Services'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
+import { HeroService } from './hero.service';
+
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
@@ -13,6 +15,9 @@ import { HeroDetailComponent } from './hero-detail.component';
   declarations: [
     AppComponent,
     HeroDetailComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [
     AppComponent
